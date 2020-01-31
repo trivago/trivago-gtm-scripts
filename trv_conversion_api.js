00f38e86f6fa19cc6778264f97b996d2e501e5fb
Nikita Zavyalov <Gaikanomer9@users.noreply.github.com>
2020-01-31 16:32:59 +0100

    Update trv_conversion_api.js

--- trv_conversion_api.js
+++ trv_conversion_api.js
@@ -27,10 +27,16 @@ TRV.Tag.doEvent = function() {
     tf_source=2;
   }
   
-  if (TRV.Tag.trv_reference=='deleted'){ 
+  if (TRV.Tag.trv_reference=="deleted"){ 
     TRV.Tag.trv_reference = trvCheckGetCookie('GTM_TRV_REFERENCE_TR');
     tf_source=3;
   }
+  
+  if (TRV.Tag.trv_reference=="deleted"){ 
+    TRV.Tag.trv_reference = localStorage.getItem('GTM_TRV_REFERENCE_TR');
+    tf_source=4;
+  }
+  
   
   let opts = {
     advertiser_id: TRV.Tag.advertiser_id,
